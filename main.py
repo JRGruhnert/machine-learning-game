d17c7ddbeb908bfd5b7c6de008bfc51db3413714
Jan Gruhnert <jan-gruhnert@gmx.de>
2023-03-11 20:44:00 +0100

    Add proper background and ai player sprite

--- main.py
+++ main.py
@@ -1,34 +1,13 @@
 import arcade
 import arcade.gui
+import const
 
-from moving_object import Agent
-
-# game const
-class GameField():
-    def __init__(self, center_x, center_y, width, height):
-        self.center_x = center_x
-        self.center_y = center_y
-        self.width = width
-        self.height = height
-        self.left_border = center_x - (width / 2)
-        self.right_border = center_x + (width / 2)
-        self.bottom_border = center_y - (height / 2)
-        self.top_border = center_y + (height / 2)
-        
-# const
-WINDOW_WIDTH = 1200
-WINDOW_HEIGHT = 800
-WINDOW_TITLE = "Machine learning game"
-  
-GAME_FIELD = GameField(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, WINDOW_WIDTH * 0.6, WINDOW_HEIGHT * 0.6)
-BACKGROUND_COLOR = arcade.color.AMAZON
-SPRITE_SCALING = 0.5
-
+from moving_object import Spaceship
 
 class Game(arcade.Window):
     def __init__(self):
         # Call the parent class and set up the window
-        super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
+        super().__init__(const.WIN_WIDTH, const.WIN_HEIGHT, const.WINDOW_TITLE)
 
 
 class GameView(arcade.View):
@@ -36,11 +15,69 @@ class GameView(arcade.View):
         super().__init__()
 
         #init all elements
-        self.player_sprite = arcade.Sprite(":resources:images/animated_characters/female_person/femalePerson_idle.png",
-                                           SPRITE_SCALING)
-        self.player_sprite.center_x = 50
-        self.player_sprite.center_y = 50
-        self.player_sprite.velocity = [3, 3]
+        self.ai_sprite = Spaceship()
+        self.background = arcade.load_texture(":resources:images/backgrounds/abstract_1.jpg")
+        x_factor = self.background.width / const.WIN_WIDTH
+        y_factor = self.background.height / const.WIN_HEIGHT
+        self.background_inner = arcade.load_texture(":resources:images/backgrounds/abstract_1.jpg", 
+                                                    const.DZ_LEFT * x_factor, const.DZ_BOTTOM * y_factor, const.DZ_WIDTH * x_factor, const.DZ_HEIGHT * y_factor)
+        #init gui elements
+        self.manager = arcade.gui.UIManager()
+        self.manager.enable()
+
+        # Render button
+        default_style = {
+            "font_name": ("calibri", "arial"),
+            "font_size": 15,
+            "font_color": arcade.color.WHITE,
+            "border_width": 2,
+            "border_color": None,
+            "bg_color": (21, 19, 21),
+
+            # used if button is pressed
+            "bg_color_pressed": arcade.color.WHITE,
+            "border_color_pressed": arcade.color.WHITE,  # also used when hovered
+            "font_color_pressed": arcade.color.BLACK,
+        }
+
+        red_style = {
+            "font_name": ("calibri", "arial"),
+            "font_size": 15,
+            "font_color": arcade.color.WHITE,
+            "border_width": 2,
+            "border_color": None,
+            "bg_color": arcade.color.REDWOOD,
+
+            # used if button is pressed
+            "bg_color_pressed": arcade.color.WHITE,
+            "border_color_pressed": arcade.color.RED,  # also used when hovered
+            "font_color_pressed": arcade.color.RED,
+        }
+
+
+        
+         # Create a vertical BoxGroup to align buttons
+        self.h_box = arcade.gui.UIBoxLayout(vertical=False)
+
+        # Create the buttons
+        start_button = arcade.gui.UIFlatButton(text="Start", width=200, style=default_style)
+        self.h_box.add(start_button.with_space_around(bottom=20, right= 10, left=10, top=20))
+
+        train_button = arcade.gui.UIFlatButton(text="Train", width=200, style=red_style)
+        self.h_box.add(train_button.with_space_around(bottom=20, right= 10, left=10, top=20))
+
+        # assign  callback
+        start_button.on_click = self.on_click_start
+        train_button.on_click = self.on_click_train
+
+     
+        # Create a widget to hold the v_box widget, that will center the buttons
+        self.manager.add(
+            arcade.gui.UIAnchorWidget(
+                anchor_x="center_x",
+                anchor_y="bottom",
+                child=self.h_box)
+        )
 
 
 
@@ -49,51 +86,50 @@ class GameView(arcade.View):
         self.bullet_sprites_list = None
 
         # Set up the player info
-        self.agent_sprite = Agent(":resources:images/animated_characters/female_person/femalePerson_idle.png", SPRITE_SCALING, GAME_FIELD)
         self.seconds_survived = 0.0
 
-
-
-
-
-
     def on_show_view(self):
-        arcade.set_background_color(arcade.color.AMAZON)
-
+        arcade.set_background_color(arcade.color.BLACK)
+        
     def on_draw(self):
         self.clear()
-
-        arcade.draw_rectangle_outline(GAME_FIELD.center_x, GAME_FIELD.center_y, GAME_FIELD.width + 2, GAME_FIELD.height + 2,
+        
+        #draw background
+        arcade.draw_lrwh_rectangle_textured(0, 0, const.WIN_WIDTH, const.WIN_HEIGHT, self.background, alpha=100)
+        arcade.draw_rectangle_outline(const.WIN_CENTER_X, const.WIN_CENTER_Y, const.DZ_WIDTH + 2, const.DZ_HEIGHT + 2,
                               arcade.color.BLACK)
-        arcade.draw_rectangle_filled(GAME_FIELD.center_x, GAME_FIELD.center_y, GAME_FIELD.width, GAME_FIELD.height,
-                              arcade.color.APPLE_GREEN)
+        arcade.draw_lrwh_rectangle_textured(const.DZ_LEFT, const.DZ_BOTTOM, const.DZ_WIDTH, const.DZ_HEIGHT, self.background_inner)
+        #arcade.draw_rectangle_filled(const.WIN_CENTER_X, const.WIN_CENTER_Y, const.DZ_WIDTH, const.DZ_HEIGHT , [255,255,255,50])
         
         # Draw all the sprites.
-        self.player_sprite.draw()
+        self.ai_sprite.draw()
 
-        # Show tip to pause screen
+        # Draw ui on top of everything
+        self.manager.draw()
         arcade.draw_text("Press Esc. to pause",
-                         WINDOW_WIDTH / 2,
-                         WINDOW_HEIGHT - 100,
+                         const.WIN_CENTER_X,
+                         const.WIN_HEIGHT - 100,
                          arcade.color.BLACK,
                          font_size=20,
                          anchor_x="center")
 
     def on_update(self, delta_time):
         # Call update on all sprites
-        self.player_sprite.update()
-
-        # Bounce off the edges
-        if self.player_sprite.left < 0 or self.player_sprite.right > WINDOW_WIDTH:
-            self.player_sprite.change_x *= -1
-        if self.player_sprite.bottom < 0 or self.player_sprite.top > WINDOW_HEIGHT:
-            self.player_sprite.change_y *= -1
+        self.ai_sprite.update()
+        
 
     def on_key_press(self, key, _modifiers):
         if key == arcade.key.ESCAPE:
             # pass self, the current view, to preserve this view's state
             pause = PauseView(self)
             self.window.show_view(pause)
+
+
+    def on_click_start(self, event):
+        print("Start:", event)
+
+    def on_click_train(self, event):
+        print("Train:", event)
 
 
 class PauseView(arcade.View):
@@ -110,29 +146,29 @@ class PauseView(arcade.View):
         # Draw player, for effect, on pause screen.
         # The previous View (GameView) was passed in
         # and saved in self.game_view.
-        player_sprite = self.game_view.player_sprite
-        player_sprite.draw()
+        ai_sprite = self.game_view.ai_sprite
+        ai_sprite.draw()
 
         # draw an orange filter over him
-        arcade.draw_lrtb_rectangle_filled(left=player_sprite.left,
-                                          right=player_sprite.right,
-                                          top=player_sprite.top,
-                                          bottom=player_sprite.bottom,
+        arcade.draw_lrtb_rectangle_filled(left=ai_sprite.left,
+                                          right=ai_sprite.right,
+                                          top=ai_sprite.top,
+                                          bottom=ai_sprite.bottom,
                                           color=arcade.color.ORANGE + (200,))
 
-        arcade.draw_text("PAUSED", WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 + 50,
+        arcade.draw_text("PAUSED", const.WIN_CENTER_X, const.WIN_CENTER_Y + 50,
                          arcade.color.BLACK, font_size=50, anchor_x="center")
 
         # Show tip to return or reset
         arcade.draw_text("Press Esc. to return",
-                         WINDOW_WIDTH / 2,
-                         WINDOW_HEIGHT / 2,
+                         const.WIN_CENTER_X,
+                         const.WIN_CENTER_Y,
                          arcade.color.BLACK,
                          font_size=20,
                          anchor_x="center")
         arcade.draw_text("Press Enter to reset",
-                         WINDOW_WIDTH / 2,
-                         WINDOW_HEIGHT / 2 - 30,
+                         const.WIN_CENTER_X,
+                         const.WIN_CENTER_Y - 30,
                          arcade.color.BLACK,
                          font_size=20,
                          anchor_x="center")
@@ -151,9 +187,9 @@ class MenuView(arcade.View):
 
     def on_draw(self):
         self.clear()
-        arcade.draw_text("Menu Screen", WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2,
+        arcade.draw_text("Menu Screen", const.WIN_CENTER_X, const.WIN_CENTER_Y,
                          arcade.color.BLACK, font_size=50, anchor_x="center")
-        arcade.draw_text("Click to advance.", WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 - 75,
+        arcade.draw_text("Click to advance.", const.WIN_CENTER_X, const.WIN_CENTER_Y - 75,
                          arcade.color.GRAY, font_size=20, anchor_x="center")
 
     def on_mouse_press(self, _x, _y, _button, _modifiers):
--- moving_object.py
+++ moving_object.py
@@ -1,47 +1,45 @@
 import arcade
+import const
 
+HEALTH = 3
+SPACESHIP_SPRITE_FILE = ":resources:images/animated_characters/female_person/femalePerson_idle.png"
+METEOR_SPRITE_FILE =  ":resources:images/animated_characters/female_person/femalePerson_idle.png"
+SPRITE_SCALING = 0.5
 
 class Moving_Object(arcade.Sprite):
-    def __init__(self, filename, sprite_scaling, game_bounds, health):
-        super().__init__(filename, sprite_scaling)
+    def __init__(self, filename):
+        super().__init__(filename, SPRITE_SCALING)
 
-        self.game_bounds = game_bounds
         self.change_x = 0
         self.change_y = 0
-        self.health = health
+        self.health = HEALTH
   
     def update(self):
 
-        # Move the coin
+        # Move
         self.center_x += self.change_x
         self.center_y += self.change_y
 
-        # If we are out-of-bounds, then 'bounce'
-        if self.left < self.game_field.left_border:
+        # Bounce off the edges
+        if self.left < const.DZ_LEFT or self.right > const.DZ_RIGHT :
             self.change_x *= -1
 
-        if self.right > self.game_field.right_border:
-            self.change_x *= -1
-
-        if self.bottom < self.game_field.bottom_border:
+        if self.bottom < const.DZ_BOTTOM or self.top > const.DZ_TOP:
             self.change_y *= -1
-
-        if self.top > self.game_field.top_border:
-            self.change_y *= -1
-    
-    def collision(self):
-        pass
+            
+        
 
 
 class Spaceship(Moving_Object):
-    def __init__(self, filename, sprite_scaling, health, game_bounds):
-        super().__init__(filename, sprite_scaling, health, game_bounds)
+    def __init__(self):
+        super().__init__(SPACESHIP_SPRITE_FILE)
 
-        self.center_x = 50
-        self.center_y = 50
+        self.center_x = const.WIN_CENTER_X
+        self.center_y = const.WIN_CENTER_Y
+        self.velocity = [3, 3]
 
     def update(self):
-        super().update(self)
+        super().update()
 
         #calculate if agnet got hit by a bullet
         #if yes game over
@@ -49,13 +47,15 @@ class Spaceship(Moving_Object):
 
 
 class Meteor(Moving_Object):
-    def __init__(self, filename, sprite_scaling, health, game_bounds, window_bounds):
-        super().__init__(filename, sprite_scaling, health, game_bounds)
+    def __init__(self):
+        super().__init__(METEOR_SPRITE_FILE)
 
-        self.center_x = window_bounds
-        self.center_y = 50
+        self.center_x = const.WIN_CENTER_X
+        self.center_y = const.WIN_CENTER_Y
         self.crossed_border = False
 
     def update(self):
-        super().update(self)
+        super().update()
+        
+        
 
